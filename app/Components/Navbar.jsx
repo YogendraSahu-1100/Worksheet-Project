76bfd9cc62db6319f97c8de9096892f1ce5a1e83
Yogendra Sahu <yogendra@ssipmt.com>
2025-02-14 18:46:56 +0530

    Chnage Setting

--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -62,26 +62,26 @@ export function Navbar() {
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
                 <Link
-                  href="#"
+                  href="work"
                   className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
                   aria-current="page"
                 >
                   Work
                 </Link>
                 <Link
-                  href="/anime"
+                  href="anime"
                   className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                 >
                   Anime
                 </Link>
                 <Link
-                  href="#"
+                  href="games"
                   className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                 >
                   Games
                 </Link>
                 <Link
-                  href="#"
+                  href="manager"
                   className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                 >
                   Manager
